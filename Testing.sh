5ddf0e1619a8be816de84f7e241d4f907dfb08d8
OSM12345 <ian.smelser@gmail.com>
2015-04-07 09:01:34 -0700

    1

---
+++ Testing.sh
@@ -0,0 +1,18 @@
+# Another new status
+num=200
+clear
+while [[ $num -le 203 ]]; do
+    echo "Pining"
+    ping -q -c3 192.168.0.$num > /dev/null
+    ret=$?
+    if [[ ret==1 ]]; then
+        echo "---------------------------------------------"
+        echo "Parallella Board ${bold}$num${normal} Status is ${bold}Online${normal}"
+        echo "---------------------------------------------"
+    else
+        echo "---------------------------------------------"
+        echo "Parallella Board ${bold}$num${normal} Status is ${bold}Offline${normal}"
+        echo "---------------------------------------------"
+    fi
+    num=$((num+1))
+fi
