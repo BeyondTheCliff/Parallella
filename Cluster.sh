4e79df6adebe5cccc702a0daa45280efec0df562
OSM12345 <ian.smelser@gmail.com>
2015-04-08 08:08:43 -0700

    Clean up formating

--- Cluster.sh
+++ Cluster.sh
@@ -7,7 +7,7 @@ normal=`tput sgr0`
 SSH ()
 { 
 	num=200
-	while [ $num -le 203 ]; do
+	while [[ $num -le 203 ]]; do
         echo $num "using" $1
 		ssh linaro@192.168.0.$num $1
 		num=$((num+1))
@@ -15,19 +15,19 @@ SSH ()
 }
 
 
-if [ "$1" != "" ]; then
-    if [ "$1" == "help" ]; then
+if [[ "$1" != "" ]]; then
+    if [[ "$1" == "help" ]]; then
     	echo "Options include:: ${bold}shutdown, tell, update, status, transfer${normal}"
-    elif [ "$1" == "shutdown" ]; then
+    elif [[ "$1" == "shutdown" ]]; then
     	echo "Shutting down cluster"
     	SSH 'echo linaro | sudo shutdown -h now'
-    elif [ "$1" == "tell" ]; then
+    elif [[ "$1" == "tell" ]]; then
     	echo "Telling cluster" $2
     	SSH $2
-    elif [ "$1" == "update" ]; then # Needs testing
+    elif [[ "$1" == "update" ]]; then # Works, now expand
     	echo "Updating cluster"
     	SSH 'cd Parallella; git pull'
-    elif [ "$1" == "status" ]; then # Done
+    elif [[ "$1" == "status" ]]; then
     	num=200
         clear
         while [[ $num -le 203 ]]; do
@@ -45,11 +45,13 @@ if [ "$1" != "" ]; then
             fi
             num=$((num+1))
         done
-    elif [ "$1" == "transfer"]; then
+    elif [[ "$1" == "transfer" ]]; then
         echo "Copying Now"
         scp $2 linaro@192.168.0.200:$3
         scp $2 linaro@192.168.0.201:$3
         scp $2 linaro@192.168.0.202:$3
+    elif [[ "$1" == "mpi start" ]]; then # Next feature
+        #statements
     else
     	echo "Bad Command"
     fi
