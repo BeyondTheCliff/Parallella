ddf585781c1855c55b577acfa073589b8bd742a6
OSM12345 <ian.smelser@gmail.com>
2015-04-11 22:01:47 -0700

    New Stats

--- Stats.sh
+++ Stats.sh
@@ -1,8 +1,9 @@
 # Status new Saturday, April 11, 2015
+# Done, moving to Cluster
 
 clear
 num=200
-echo "Starting"
+echo "    Starting"
 echo "----------------------------"
 while [[ $num -le 203 ]]; do
 	ssh linaro@192.168.0.$num ip addr show | grep "192.168."
