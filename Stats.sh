2b4e12a37fc50cb18a290251fc33a470d1f8b5a4
OSM12345 <ian.smelser@gmail.com>
2015-04-11 22:00:32 -0700

    New Stats

--- Stats.sh
+++ Stats.sh
@@ -3,7 +3,9 @@
 clear
 num=200
 echo "Starting"
+echo "----------------------------"
 while [[ $num -le 203 ]]; do
-	ssh linaro@192.168.0.$num ip addr show | grep "inet"
+	ssh linaro@192.168.0.$num ip addr show | grep "192.168."
+	echo "----------------------------"
 	num=$((num+1))
 done
