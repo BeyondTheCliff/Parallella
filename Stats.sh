d340fb33f1451ea5c2f707355104a79386837ec9
OSM12345 <ian.smelser@gmail.com>
2015-04-11 21:55:51 -0700

    New Stats

--- Stats.sh
+++ Stats.sh
@@ -1,4 +1,15 @@
 # Status new Saturday, April 11, 2015
+
+SSH ()
+{ 
+	num=200
+	while [[ $num -le 203 ]]; do
+        echo $num "using" $1
+		ssh linaro@192.168.0.$num $1
+		num=$((num+1))
+	done
+}
+
 clear
 num=200
 echo "Starting"
