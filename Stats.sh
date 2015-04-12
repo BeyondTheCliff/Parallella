515f839aa7f019b234e553415e474238a34da67c
OSM12345 <ian.smelser@gmail.com>
2015-04-11 21:58:06 -0700

    New Stats

--- Stats.sh
+++ Stats.sh
@@ -1,19 +1,9 @@
 # Status new Saturday, April 11, 2015
 
-SSH ()
-{ 
-	num=200
-	while [[ $num -le 203 ]]; do
-        echo $num "using" $1
-		ssh linaro@192.168.0.$num $1
-		num=$((num+1))
-	done
-}
-
 clear
 num=200
 echo "Starting"
 while [[ $num -le 203 ]]; do
-	SSH 'ip addr show | grep'
+	ssh linaro@192.168.0.$num ip addr show | grep "inet"
 	num=$((num+1))
 done
