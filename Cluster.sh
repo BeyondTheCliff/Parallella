f69cf1d8b3f1071f78d0fac09b90f520b8c337f7
OSM12345 <ian.smelser@gmail.com>
2015-04-16 18:29:32 -0700

    new less broken

--- Cluster.sh
+++ Cluster.sh
@@ -1,21 +1,26 @@
 # Actual management system for the local computer cluster using Parallella.
 # Designed for master parallella board (203)
 # Fork: Status
-bold=`tput bold`
-normal=`tput sgr0`
+b=`tput bold`
+n=`tput sgr0`
 
 ipname=(102,103,104,105)
 
 SSH ()
 { 
+    echo "----------------------------"
     ssh linaro@192.168.0.${ipname[1]} $1
+    echo "----------------------------"
     ssh linaro@192.168.0.${ipname[2]} $1
+    echo "----------------------------"
     ssh linaro@192.168.0.${ipname[3]} $1
+    echo "----------------------------"
     ssh linaro@192.168.0.${ipname[4]} $1
+    echo "----------------------------"
 }
 if [[ "$1" != "" ]]; then
     if [[ "$1" == "help" ]]; then
-    	echo "Options include:: ${bold}shutdown, tell, update, status, transfer${normal}"
+    	echo "Options include:: ${b}shutdown, tell, update, status, transfer${n}"
     elif [[ "$1" == "shutdown" ]]; then
     	echo "Shutting down cluster"
     	SSH 'echo linaro | sudo shutdown -h now'
@@ -27,17 +32,10 @@ if [[ "$1" != "" ]]; then
     	SSH 'cd Parallella; git pull'
     elif [[ "$1" == "status" ]]; then
     	clear
-        num=0
         echo "----------------------------"
         echo "    Starting"
         echo "----------------------------"
-        while [[ $num -le 3 ]]; do
-            ssh linaro@192.168.0.$ipname ip addr show | grep "eth0"
-            echo "----------------------------"
-            num=$((num+1))
-        done
-            
-        done
+        SSH ip addr show | grep "eth0"
     elif [[ "$1" == "transfer" ]]; then
         echo "Copying Now"
         scp $2 linaro@192.168.0.200:$3
