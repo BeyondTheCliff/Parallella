ef5f4e32f90f9623534650a62cf32236c3f57c82
OSM12345 <ian.smelser@gmail.com>
2015-04-16 18:39:01 -0700

    new less broken

--- Cluster.sh
+++ Cluster.sh
@@ -9,13 +9,13 @@ ipname=(102,103,104,105)
 SSH ()
 { 
     echo "----------------------------"
-    ssh linaro@192.168.0.$ipname[1] $1
+    ssh linaro@192.168.0.${ipname[1]} $1
     echo "----------------------------"
-    ssh linaro@192.168.0.$ipname[2] $1
+    ssh linaro@192.168.0.${ipname[2]} $1
     echo "----------------------------"
-    ssh linaro@192.168.0.$ipname[3] $1
+    ssh linaro@192.168.0.${ipname[3]} $1
     echo "----------------------------"
-    ssh linaro@192.168.0.$ipname[4] $1
+    ssh linaro@192.168.0.${ipname[4]} $1
     echo "----------------------------"
 }
 if [[ "$1" != "" ]]; then
